f661ced4be8ccb4edb8c616c7864fef7cc7f8fe6
Elodie COCQ <cocqelodie@hotmail.com>
2017-01-29 23:33:00 -0500

    Reprise de tout

--- 3-Composants/sceneObject.js
+++ 3-Composants/sceneObject.js
@@ -17,80 +17,32 @@ define([
       this.children = [];
     }
 
-    static create(name, description, owner) {
-        const sceneObj = new SceneObject();
-        sceneObj.name = name;
-        sceneObj.owner = owner;
-        sceneObj.description = description;
-        return sceneObj;
-    }
-
-    setup() {
-      var currentObj = this;
-      var action = function(resolve) {
-        for(var i in currentObj.description['components']) {
-           //var comp = {type : i, descr : currentObj.description['components'][i]}
-           currentObj.addComponent(i);
-        }
-
-        for(var i in currentObj.description['children']) {
-
-          currentObj.addChild(i, currentObj.description['children'][i]);
-        }
-        /* return Promise.all(currentObj.children.map(function(sceneObj) {
-             return sceneObj.setup();
-        })).then(function() {
-             resolve(currentObj.owner);
-        });*/
-        resolve(currentObj.owner);
-      }
-
-      return new Promise(action);
-    }
-
     addComponent(type) {
       const newComponent = ComponentFactory.create(type, this);
-      if(this.description) {
-        for(var i in this.description['components']) {
-             if(type == i) {
-               console.log("setup");
-                newComponent.setup(this.description['components'][i]);
-                break;
-             }
-        }
-      }
-
-      this.components.push({type : type, obj : newComponent});
+      this.components[type] = newComponent;
+      return newComponent;
     }
 
     // ## Fonction *getComponent*
     // Cette fonction retourne un composant existant du type spécifié
     // associé à l'objet.
     getComponent(type) {
-      for(var i in this.components) {
-        if(this.components[i].type == type) return this.components[i].obj;
-      }
-      return null;
+      return this.components[type];
     }
 
     // ## Méthode *addChild*
     // La méthode *addChild* ajoute à l'objet courant un objet
     // enfant.
     addChild(objectName, child) {
-      var myChild = SceneObject.create(objectName, child, this.owner);
-      myChild.setup();
-      this.children.push(myChild);
-
+      //var myChild = SceneObject.create(objectName, child, this.owner);
+      this.children[objectName]=child;
     }
 
     // ## Fonction *getChild*
     // La fonction *getChild* retourne un objet existant portant le
     // nom spécifié, dont l'objet courant est le parent.
     getChild(objectName) {
-      for(var i in this.children) {
-        if(this.children[i].name == objectName) return this.children[i]; //.description pour avoir juste le contenu
-      }
-      return null;
+        return this.children[objectName];
     }
 
     // ## Fonction *fingObjectInScene*
@@ -99,32 +51,16 @@ define([
     // avec l'objet courant. Voir la méthode `findObject` de la
     // classe *Scene*.
     findObjectInScene(objectName) {
-      return this.owner.findObject(objectName);
+      return window.scene.findObject(objectName);
     }
 
-    findObjectInChildren(objectName) {
-        if(this.name == objectName) return this;
-        if(this.children.length > 0) {
-          for(var i in this.children) {
-             if(this.children[i].name == objectName) return this.children[i];
-             else {
-                 var res = this.children[i].findObjectInChildren(objectName);
-                 if(res) return res;
-             }
-          }
-        }
-        return null;
-    }
 
     // ## Méthode *display*
     // Cette méthode appelle la méthode *display* des composants
     // de l'objet.
     display(dT) {
       for(var i in this.components) {
-        this.components[i].obj.display(dT);
-      }
-      for(var i in this.children) {
-        this.children[i].display(dT);
+        this.components[i].display(dT);
       }
     }
 
@@ -133,10 +69,7 @@ define([
     // de l'objet.
     update(dT) {
       for(var i in this.components) {
-        this.components[i].obj.update(dT);
-      }
-      for(var i in this.children) {
-        this.children[i].update(dT);
+        this.components[i].update(dT);
       }
     }
   }
